a6504258a75ff0a961f33ede8184c742ad00893d
Methanan <matanun.first@mail.kmutt.ac.th>
2022-09-08 18:49:06 +0700

    Bmi

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -2,8 +2,19 @@ const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
   //code here
+  let bmi = weight/(height*height)
+  return bmi ;
+  
 }
 function getBMIMeaning(weight, height) {
   //code here
+  let bmi = calculateBMI(weight,height)
+  if (bmi < 18.5) {
+    return "Underweight";
+  } else if (bmi > 18.5 && bmi < 24.9) {
+    return "Normal weight";
+  } else if (bmi > 25.0){
+    return "Overweight";
+  }
 }
 module.exports = getBMIMeaning
